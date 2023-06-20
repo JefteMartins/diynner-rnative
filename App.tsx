5a5a5bdef85908e02330f37e3a7c06dd27aa6d35
JefteMartins <jefte.martins@hotmail.com>
2023-06-20 19:48:32 -0300

    instalação e teste de navegação

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Home from './src/screens/home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ImagesHome from './src/components/imagesHome';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Home />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home' >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ImagesHome" component={ImagesHome} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,17 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Button } from 'react-native';
 import { styles } from '../../components/styles';
 import ImagesHome from '../../components/imagesHome';
 import Toolbar from '../../components/toolbar';
 
-export default function Home() {
+export default function Home({ navigation }: any) {
   return (
     <View style={styles.container} >
       <StatusBar style="auto" />
 
-			<Toolbar />
-			{/* componente dos cards */}
+      <Toolbar />
+      {/* componente dos cards */}
       <ImagesHome />
+      <Button title="Go to Details" onPress={() => navigation.navigate('ImagesHome')} />
     </View>
   );
 }
