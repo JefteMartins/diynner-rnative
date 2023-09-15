fb988e3b3d74169d3ae1909caab33ea2b8704251
Jefte Martins <jefte.martins@hotmail.com>
2023-09-15 17:16:24 -0300

    guardar a informação da comrpa no firestore database

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,4 +14,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
--- src/screens/home/cart.tsx
+++ src/screens/home/cart.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from 'native-base';
 import CardRecipe from '../../components/cardRecipe';
 import { styles } from '../stylesScreen';
+import firestore, { addDoc, collection } from 'firebase/firestore';
+import { FIREBASE_DB } from '../../../FirebaseConfig';
+
 
 const PaymentScreen = (props: any) => {
     const navigation = useNavigation();
@@ -13,7 +16,15 @@ const PaymentScreen = (props: any) => {
     foodIngredientsData.forEach((element: { value: any; }) => {
         valor += element.value;
     });
-    const handlePayment = () => {
+    console.log(foodDetailsData);
+    
+    
+    const handlePayment = async () => {
+        //add only foodDetailsData.nome in a collection
+        const docRef = addDoc(collection(FIREBASE_DB, 'compras'),{
+            nome: foodDetailsData.nome
+        });
+        console.log('compra cadastrada', docRef)
         navigation.navigate('Home');
     };
 
