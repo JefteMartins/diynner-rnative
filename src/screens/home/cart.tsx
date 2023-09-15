990b88f9f38c2c5ef428bee55f03c43a502ad0ba
Jefte Martins <jefte.martins@hotmail.com>
2023-09-15 17:43:24 -0300

    add coleção de cada user

--- src/screens/home/cart.tsx
+++ src/screens/home/cart.tsx
@@ -5,23 +5,21 @@ import { Button } from 'native-base';
 import CardRecipe from '../../components/cardRecipe';
 import { styles } from '../stylesScreen';
 import firestore, { addDoc, collection } from 'firebase/firestore';
-import { FIREBASE_DB } from '../../../FirebaseConfig';
-
+import { FIREBASE_AUTH, FIREBASE_DB } from '../../../FirebaseConfig';
+import firebase from 'firebase/app';
 
 const PaymentScreen = (props: any) => {
     const navigation = useNavigation();
     const foodDetailsData = props.route.params;
     const foodIngredientsData = foodDetailsData.recipe;
+    const userEmail = FIREBASE_AUTH.currentUser?.email;
     let valor = 0;
     foodIngredientsData.forEach((element: { value: any; }) => {
         valor += element.value;
     });
     console.log(foodDetailsData);
-    
-    
     const handlePayment = async () => {
-        //add only foodDetailsData.nome in a collection
-        const docRef = addDoc(collection(FIREBASE_DB, 'compras'),{
+        const docRef = addDoc(collection(FIREBASE_DB, `${userEmail}`),{
             nome: foodDetailsData.nome
         });
         console.log('compra cadastrada', docRef)
